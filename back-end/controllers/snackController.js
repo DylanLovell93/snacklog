ad8c81f68e490c0d5c15e9ff46e5c6a470fcc899
DylanLovell <arcanyne7@gmail.com>
2022-02-12 17:57:52 -0500

    /snacks/id route

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -4,9 +4,15 @@ const express = require('express');
 //create our controller
 const snackController = express.Router();
 
-//create "/" route
+//create "/" get route
 snackController.get('/', (_, response) => {
   response.status(200).json({ route: '/snacks' });
+});
+
+//create "/:id" get route
+snackController.get('/', (request, response) => {
+  const { id } = request.params;
+  response.status(200).json({ route: `/snacks/${id}` });
 });
 
 // export our controller
