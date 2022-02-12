887af6fa8ac5489c9cd6def2f0e4d238be3608af
vanessaconti <vconti901@aol.com>
2022-02-11 19:09:17 -0500

    started app file on backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,21 @@
 // DEPENDENCIES
-
 const express = require("express");
+const cors = require("cors");
+//const snackController = require("./controllers/snackController")
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//app.use("/snacks", snackController)
 
 // ROUTES
+app.get("/", (_, response) => {
+  response.status(200).send("Welcome to our Snack App");
+});
 
 // EXPORT
 module.exports = app;
