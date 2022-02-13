3da5cc13b60537f828da6dc33aab0d6f3e818163
DylanLovell <arcanyne7@gmail.com>
2022-02-12 19:34:50 -0500

    /snacks post route

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -6,19 +6,25 @@ const snackController = express.Router();
 
 //create "/" get route
 snackController.get('/', (_, response) => {
-  response.status(200).json({ route: '/snacks route' });
+  response.status(200).json({ route: '/snacks get route' });
 });
 
 //create "/:id" get route
 snackController.get('/:id', (request, response) => {
   const { id } = request.params;
-  response.status(200).json({ route: `/snacks/${id} route` });
+  response.status(200).json({ route: `/snacks/${id} get route` });
 });
 
 //create "/:id" delete route
 snackController.delete('/:id', (request, response) => {
   const { id } = request.params;
   response.status(200).json({ route: `/snacks/${id} delete route` });
+});
+
+//create "/" post route
+snackController.post('/:id', (request, response) => {
+  const { body: newPost } = request;
+  response.status(200).json({ route: '/snacks post route' });
 });
 
 // export our controller
