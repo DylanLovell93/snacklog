3098937b9aabe920ee0c111ccb734b8fc013b614
vanessaconti <vconti901@aol.com>
2022-02-15 04:33:09 -0500

    pages and appjs updated changed navbar imports need to come back to indexhtml and favicon

---
+++ front-end/src/Pages/Edit.js
@@ -0,0 +1,16 @@
+import EditForm from "../Components/EditForm";
+import NavBar from "../Components/NavBar";
+
+function Edit() {
+  return (
+    <>
+      <NavBar />
+      <div className="Edit">
+        <h2>Edit</h2>
+        <EditForm />
+      </div>
+    </>
+  );
+}
+
+export default Edit;
